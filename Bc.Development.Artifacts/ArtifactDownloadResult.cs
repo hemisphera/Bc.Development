4a75c45be50fee2eb76138074fb9e2337649a20d
Thomas Torggler <thomas.torggler@eos-solutions.it>
2023-10-03 11:26:17 +0200

    Allow tagging "lastused" from download result

--- Bc.Development.Artifacts/ArtifactDownloadResult.cs
+++ Bc.Development.Artifacts/ArtifactDownloadResult.cs
@@ -1,4 +1,7 @@
-﻿namespace Bc.Development.Artifacts
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Bc.Development.Artifacts
 {
 
   public class ArtifactDownloadResult
@@ -7,6 +10,21 @@
     public BcArtifact Artifact { get; set; }
 
     public BcArtifact PlatformArtifact { get; set; }
+
+
+    public async Task SetLastUsedDate(DateTime? tag = null)
+    {
+      await Task.WhenAll(
+        Task.Run(async () =>
+        {
+          if (Artifact != null) await Artifact.SetLastUsedDate(tag);
+        }),
+        Task.Run(async () =>
+        {
+          if (PlatformArtifact != null) await PlatformArtifact.SetLastUsedDate(tag);
+        })
+      );
+    }
 
   }
 
