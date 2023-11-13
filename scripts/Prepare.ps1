16a1b88d37d19d64262210af0e2d2dc19150fc63
Thomas Torggler <thomas.torggler@eos-solutions.it>
2023-11-13 17:39:59 +0100

    Fix: create assembly folder if it does not exist

--- scripts/Prepare.ps1
+++ scripts/Prepare.ps1
@@ -1,5 +1,6 @@
 $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
 $ExternalAssembliesFolder = Join-Path $ProjectRoot "_external"
+[IO.Directory]::CreateDirectory($ExternalAssembliesFolder) | Out-Null
 
 $FrameworkUiClientPath = "$ExternalAssembliesFolder/Microsoft.Dynamics.Framework.UI.Client.dll"
 Invoke-WebRequest $env:FRAMEWORK_UI_CLIENT_URI -OutFile $FrameworkUiClientPath
