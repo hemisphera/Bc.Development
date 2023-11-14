7662e37192f65d6953b70c2732148e9b2eec2ced
Thomas Torggler <thomas.torggler@eos-solutions.it>
2023-11-14 09:39:59 +0100

    CachedCredentialReader revision

--- Bc.Development.Configuration/AlLanguageExtension.cs
+++ Bc.Development.Configuration/AlLanguageExtension.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 
 namespace Bc.Development.Configuration
 {
+  /// <summary>
+  /// Provides information about an AL Language Extension.
+  /// </summary>
   public class AlLanguageExtension
   {
     private static AlLanguageExtension[] All { get; set; }
 
+    /// <summary>
+    /// Returns a list of all AL Language Extensions installed on the system.
+    /// </summary>
+    /// <param name="force">If true, the list is reloaded from the file system, even if it was already loaded.</param>
+    /// <returns>The list of AL Language Extensions.</returns>
     public static AlLanguageExtension[] Enumerate(bool force = false)
     {
       if (All == null || force)
@@ -39,6 +45,11 @@ namespace Bc.Development.Configuration
       }).Where(a => a != null).ToArray();
     }
 
+    /// <summary>
+    /// Returns the latest version of the AL Language Extension for the specified platform.
+    /// </summary>
+    /// <param name="platform">The platform to return the latest version for. If null, the latest version for any platform is returned.</param>
+    /// <returns>The latest version of the AL Language Extension for the specified platform.</returns>
     public static AlLanguageExtension GetLatest(string platform = null)
     {
       return Enumerate()
@@ -47,40 +58,36 @@ namespace Bc.Development.Configuration
         .LastOrDefault();
     }
 
-
+    /// <summary>
+    /// The folder where the AL Language Extension is installed.
+    /// </summary>
     public DirectoryInfo Folder { get; }
 
+    /// <summary>
+    /// The full path to the alc.exe file.
+    /// </summary>
     public string AlcPAth => Path.Combine(Folder.FullName, "alc.exe");
 
+    /// <summary>
+    /// The version of the AL Language Extension.
+    /// </summary>
     public Version FileVersion { get; set; }
 
+    /// <summary>
+    /// The platform the AL Language Extension is for. 
+    /// </summary>
     public string Platform => Folder.Name;
 
 
+    /// <summary>
+    /// Creates a new instance of the AlLanguageExtension class.
+    /// </summary>
+    /// <param name="folder">The folder where the AL Language Extension is installed.</param>
     public AlLanguageExtension(string folder)
     {
       Folder = new DirectoryInfo(folder);
       if (!File.Exists(AlcPAth)) throw new NotSupportedException();
       FileVersion = Version.Parse(FileVersionInfo.GetVersionInfo(AlcPAth).FileVersion);
-    }
-
-
-    public NetworkCredential GetStoredCredential(string key)
-    {
-      var reader = CredentialReader.CreateUserPasswordCache(Folder.FullName);
-      return reader.TryGetSavedCredentials(key);
-    }
-
-    public NetworkCredential GetStoredCredential(string server, string serverInstance)
-    {
-      var reader = CredentialReader.CreateUserPasswordCache(Folder.FullName);
-      return reader.TryGetSavedCredentials(server, serverInstance);
-    }
-
-    public IEnumerable<string> ListStoredCredentialKeys()
-    {
-      var reader = CredentialReader.CreateUserPasswordCache(Folder.FullName);
-      return reader.ListStoredCredentials();
     }
   }
 }
---
+++ Bc.Development.Configuration/CachedCredentialReader.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace Bc.Development.Configuration
+{
+  /// <summary>
+  /// Provides access to the credentials cached locally in the AL language extension.
+  /// </summary>
+  public class CachedCredentialReader
+  {
+    /// <summary>
+    /// The filename to the credential cache.
+    /// </summary>
+    public string Filename { get; }
+
+
+    private CachedCredentialReader(string filename)
+    {
+      Filename = filename;
+    }
+
+    /// <summary>
+    /// Creates a credential reader from a given cache file.
+    /// </summary>
+    /// <param name="filename">The full path to the credential cache file.</param>
+    /// <returns>The credential reader.</returns>
+    public static CachedCredentialReader Get(string filename)
+    {
+      return new CachedCredentialReader(filename);
+    }
+
+    /// <summary>
+    /// Creates a credential reader from the username/password cache file of a given folder.
+    /// </summary>
+    /// <param name="folder">The full path to folder of the AL language extension.</param>
+    /// <returns>The credential reader.</returns>
+    public static CachedCredentialReader GetUserPasswordCache(string folder)
+    {
+      const string filename = "UserPasswordCache.dat";
+      var fullPath = Path.Combine(folder, filename);
+      return !File.Exists(fullPath) ? null : new CachedCredentialReader(filename);
+    }
+
+    /// <summary>
+    /// Creates a credential reader from the server info cache file of a given folder.
+    /// </summary>
+    /// <param name="folder">The full path to folder of the AL language extension.</param>
+    /// <returns>The credential reader.</returns>
+    public static CachedCredentialReader GetServerInfoCache(string folder)
+    {
+      const string filename = "ServerInfoCache.dat";
+      var fullPath = Path.Combine(folder, filename);
+      return !File.Exists(fullPath) ? null : new CachedCredentialReader(filename);
+    }
+
+    /// <summary>
+    /// Creates a credential reader from the token cache file of a given folder.
+    /// </summary>
+    /// <param name="folder">The full path to folder of the AL language extension.</param>
+    /// <returns>The credential reader.</returns>
+    public static CachedCredentialReader GetTokenKeyCache(string folder)
+    {
+      const string filename = "TokenKeyCache.dat";
+      var fullPath = Path.Combine(folder, filename);
+      return !File.Exists(fullPath) ? null : new CachedCredentialReader(filename);
+    }
+
+
+    private static Uri NormalizeServerUri(Uri serverUri)
+    {
+      return new UriBuilder
+      {
+        Scheme = serverUri.Scheme,
+        Host = serverUri.Host
+      }.Uri;
+    }
+
+    /// <summary>
+    /// Creates a credential key for a given server uri and server instance.
+    /// </summary>
+    /// <param name="serverUri">The server URI.</param>
+    /// <param name="serverInstance">The server instance.</param>
+    /// <returns>The credential key.</returns>
+    public static string GetUserPasswordCredentialKey(Uri serverUri, string serverInstance)
+    {
+      return NormalizeServerUri(serverUri).ToString().ToLowerInvariant() + "_" + serverInstance.ToLowerInvariant();
+    }
+
+    /// <summary>
+    /// Reads the stored credential for a given key.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns>The credential or null if not found.</returns>
+    public NetworkCredential TryGetCredential(string key)
+    {
+      var dictionary = new UserProtectedFileStorage(Filename).Read<Dictionary<string, UsernamePassword>>();
+      if (dictionary == null)
+        return null;
+      return dictionary.TryGetValue(key, out var savedCredentials) ? savedCredentials.AsNetworkCredential() : null;
+    }
+
+    /// <summary>
+    /// List all keys in the credential cache.
+    /// </summary>
+    /// <returns>The keys.</returns>
+    public IEnumerable<string> ListKeys()
+    {
+      var dictionary = new UserProtectedFileStorage(Filename).Read<Dictionary<string, UsernamePassword>>();
+      return dictionary.Keys;
+    }
+  }
+}
