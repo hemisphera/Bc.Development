f6369a55c355de5c2bcd11eadf5dd10a0faf5940
The Dumb <info@hemisphera.com>
2025-05-05 10:39:22 +0200

    Remove TestRunner

--- scripts/Prepare.ps1
+++
@@ -1,7 +0,0 @@
-$ProjectRoot = Resolve-Path "$PSScriptRoot\.."
-$ExternalAssembliesFolder = Join-Path $ProjectRoot "_external"
-[IO.Directory]::CreateDirectory($ExternalAssembliesFolder) | Out-Null
-
-$FrameworkUiClientPath = "$ExternalAssembliesFolder/Microsoft.Dynamics.Framework.UI.Client.dll"
-Invoke-WebRequest $env:FRAMEWORK_UI_CLIENT_URI -OutFile $FrameworkUiClientPath
-Unblock-File $FrameworkUiClientPath
